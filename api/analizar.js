325315cee12ce959ac8b7b512039f40f4dfe1e81
Ignacio <sistemas@raitrai.cl>
2025-05-29 21:31:24 -0400

    Update analizar.js

--- api/analizar.js
+++ api/analizar.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 import formidable from "formidable";
 import fs from "fs/promises";
-import pdf from "pdf-parse";
+import { default as pdf } from "pdf-parse/lib/pdf.js";
 
 export const config = {
   api: { bodyParser: false }
