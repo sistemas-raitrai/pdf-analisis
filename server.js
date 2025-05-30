ab5a33b25f4cc63c9e61d23a0ff50c5f3f2f3fef
Ignacio <sistemas@raitrai.cl>
2025-05-29 21:43:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ import analizar from "./api/analizar.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.post("/api/analizar", analizar); // Usa tu función como handler directo
-
+app.post("/api/analizar", analizar);
 app.listen(PORT, () => {
   console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
 });
